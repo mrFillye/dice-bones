c539ade6b98390f7d55097a4febd312894f99c9b
andrew.f1 <andrew.f@wellnuts.by>
2024-03-01 14:19:46 +0300

    rm login

--- apps/preview/src/app/page.tsx
+++ apps/preview/src/app/page.tsx
@@ -1,24 +1,12 @@
-import Link from 'next/link'
-import { Button } from '~/flowbite'
+'use client'
 
-const gameInfo = [
-  {
-    name: 'Dice-Online',
-    link: '/demos/dice-online',
-  },
-]
+import { redirect } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Page() {
-  return (
-    <main className="pt-10 flex flex-col justify-center items-center">
-      <h1 className="text-3xl mb-10">Choose</h1>
-      <div className="flex flex-col space-y-5">
-        {gameInfo.map((game, index) => (
-          <Link key={index} href={game.link}>
-            <Button className="w-48">{game.name}</Button>
-          </Link>
-        ))}
-      </div>
-    </main>
-  )
+  useEffect(() => {
+    redirect('/dice-online')
+  }, [])
+
+  return <></>
 }
