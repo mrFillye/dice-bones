2b86748097726745bd3c12968dd1ae771754b3d7
andrew.f1 <andrew.f@wellnuts.by>
2024-05-18 15:34:51 +0300

    fix padding

--- apps/preview/src/app/dice-online/page.tsx
+++ apps/preview/src/app/dice-online/page.tsx
@@ -1,12 +1,14 @@
 import dynamic from 'next/dynamic'
 
+import './index.css'
+
 const DiceOnline = dynamic(() => import('~/page/dice-online'), {
   ssr: false,
 })
 
 export default function Page() {
   return (
-    <main className="pt-10 flex flex-col justify-center items-center">
+    <main className="dice-online flex flex-col justify-center items-center">
       <DiceOnline />
     </main>
   )
